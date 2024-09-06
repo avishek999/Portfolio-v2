49a6dfc4cac11f7e97ae74227d370b856ab6514e
Avishek Prasad <avishekprasad0999@gmail.com>
2024-09-06 09:58:59 +0530

    added

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -24,7 +24,7 @@ export function ThreeDCardDemo() {
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <Image
-          
+           src=""
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
